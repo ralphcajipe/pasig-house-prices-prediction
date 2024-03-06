e0d095f3aa613089c11df791882a84ff9ad174c1
Stephencamilon <113228328+Stephencamilon@users.noreply.github.com>
2024-03-06 22:20:16 +0800

    preprocess.py, pasig_houses.csv, and test.csv are staged for commit. PH_houses_Pasig.csv is deleted and house/ is untracked.

---
+++ preprocess.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Load your data
+df = pd.read_csv('data/PH_houses_v2.csv')
+
+df.columns
+df.dtypes
+df.describe()
+df.isnull().sum()
+
+# Drop the 'descriptions' and 'link' columns
+df = df.drop(columns=['Description', 'Link'])
+
+# Filter rows where 'Location' is 'Pasig'
+df_pasig = df[df['Location'] == 'Pasig']
+
+# Save the dataframe with only 'Pasig' locations to a new csv file
+df_pasig.to_csv('data/PH_houses_Pasig.csv', index=False)
